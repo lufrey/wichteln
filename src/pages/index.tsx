74389901c46edc5f297e061a5e11ba31ac721e52
Lukas Frey <lukas@berenzheinzmann.com>
2022-12-06 11:53:06 +0100

    add geschenk page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,13 @@ import { signUpSchema } from "../types/account";
 import type { z } from "zod";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
   const signUp = trpc.account.signUp.useMutation();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({
     resolver: zodResolver(signUpSchema),
   })
@@ -44,46 +44,31 @@ const Home: NextPage = () => {
           <div className="text-2xl p-4">
             <form className="flex flex-col gap-6 text-white" onSubmit={handleSubmit(details => {
               signUp.mutate(details as z.infer<typeof signUpSchema>);
+              reset();
             })}>
               <div>
                 <label htmlFor="name" className="block ">Name</label>
-                <input id="name" className="px-2 rounded-sm mt-2 bg-white/10 outline-none" type="text" {...register("name")} />
+                <input id="name" className="px-2 w-full rounded-md mt-2 bg-white/10 outline-none" type="text" {...register("name")} />
                 {errors.name?.message && <div className="text-sm mt-2">{`${errors.name?.message ?? ''}`}</div>}
               </div>
               <div>
                 <label htmlFor="email" className="block ">E-Mail</label>
-                <input id="email" className="px-2 rounded-sm mt-2 bg-white/10 outline-none" type="text" {...register("email")} />
+                <input id="email" className="px-2 w-full rounded-md mt-2 bg-white/10 outline-none" type="text" {...register("email")} />
                 {errors.email?.message && <div className="text-sm mt-2">{`${errors.email?.message ?? ''}`}</div>}
               </div>
-              <button type="submit" className="px-4 py-2 rounded-sm bg-white/10 hover:bg-white/20 transition-colors">Anmelden</button>
+              <button type="submit" className="px-4 py-2 rounded-md bg-white/10 hover:bg-white/20 transition-colors">Teilnehmen</button>
+              {signUp.error ? <div className="text-sm text-center">{signUp.error?.message}</div> : null}
+              {signUp.data ? <div className="text-sm text-center">{signUp.data?.result}</div> : null}
             </form>
           </div>
 
-          {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div> */}
-          {/* <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p> */}
+
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="/geschenk"
+          >
+            <h3 className="text-2xl font-bold">Geschenkbuddy finden →</h3>
+          </Link>
         </div>
       </main>
     </>
--- src/server/trpc/router/account.ts
+++ src/server/trpc/router/account.ts
@@ -1,14 +1,45 @@
+import { TRPCError } from "@trpc/server";
 import { signUpSchema } from "../../../types/account";
 
 import { router, publicProcedure } from "../trpc";
 
 export const accountRouter = router({
-  signUp: publicProcedure.input(signUpSchema).mutation(({ input }) => {
-    return {
-      greeting: `Hello ${input?.name ?? "world"}`,
-    };
+  signUp: publicProcedure
+    .input(signUpSchema)
+    .mutation(async ({ input, ctx }) => {
+      // skip if user exists
+      if (
+        await ctx.prisma.user.findFirst({
+          where: {
+            email: input.email,
+          },
+        })
+      ) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Diese E-Mail Adresse wird bereits genutzt.",
+        });
+      }
+
+      // create new user
+      await ctx.prisma.user.create({
+        data: input,
+      });
+
+      return {
+        result: `Nutzer "${input.name}" angelegt!`,
+      };
+    }),
+  getBuddy: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.user.findFirst({
+      where: {
+        name: "Lukas",
+      },
+    });
   }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+  getAllNames: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findMany({
+      select: { name: true },
+    });
   }),
 });
