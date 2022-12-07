7bce089f52e8a874143895d03963b7b8d6f03be4
Lukas Frey <lukas@berenzheinzmann.com>
2022-12-07 21:41:18 +0100

    try to fix heading on small devices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-8 md:px-4 py-16 ">
-          <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] break-words max-w-full">
             Wichtiges-<span className="text-[hsl(280,100%,70%)]">Weihnachts</span>-Wichteln
             <br />
             <span className="block mt-2">🎅🧑‍🎄🤶</span>
