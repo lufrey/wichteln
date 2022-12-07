e577cf9e141c6a2271cce9bf07fa522f2d3c85d7
Lukas Frey <lukas@berenzheinzmann.com>
2022-12-07 21:04:39 +0100

    add correct favicon

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ["/", "/((?!api|_next/static|favicon.ico).*)"],
+  matcher: ["/", "/((?!api|_next/static|favicon.svg).*)"],
 };
 
 export function middleware(req: NextRequest) {
